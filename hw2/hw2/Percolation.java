051dac61481a921d8826f7b46e9792e807deef89
zf72 <zf72@duke.edu>
2024-01-21 22:27:43 -0500

    Day21-23: 820min

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -1,5 +1,4 @@
 package hw2;
-import static org.junit.Assert.*;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 import java.awt.*;
@@ -8,9 +7,14 @@ public class Percolation {
     private int N;
     private int[][] items;
     private int openSites;
-    private WeightedQuickUnionUF uf;
+    private WeightedQuickUnionUF ufForP; // check percolates
+    private WeightedQuickUnionUF ufForF; // check isFull
     private int topSite;
     private int bottomSite;
+    // one trap here is that bottomSite is only used for percolates
+    // and, it will hinder the differentiation of isFull
+    // so, we keep one uf with it and another without it
+
 
     // create N-by-N grid, with all sites initially blocked
     private int getIndex(int row, int col) {
@@ -31,7 +35,8 @@ public class Percolation {
             }
         }
         // connect topNode and bottomNode
-        uf = new WeightedQuickUnionUF(N * N + 2); // according to the spoilers
+        ufForP = new WeightedQuickUnionUF(N * N + 2);
+        ufForF = new WeightedQuickUnionUF(N * N + 1);
         topSite = N * N;
         bottomSite = N * N + 1;
     }
@@ -48,21 +53,27 @@ public class Percolation {
             openSites++;
             // check for upper & lower & left & right neighbour
             if (row > 0 && isOpen(row - 1, col)) {
-                uf.union(getIndex(row, col), getIndex(row - 1, col));
+                ufForP.union(getIndex(row, col), getIndex(row - 1, col));
+                ufForF.union(getIndex(row, col), getIndex(row - 1, col));
             }
             if (row < N - 1 && isOpen(row + 1, col)) {
-                uf.union(getIndex(row, col), getIndex(row + 1, col));
+                ufForP.union(getIndex(row, col), getIndex(row + 1, col));
+                ufForF.union(getIndex(row, col), getIndex(row + 1, col));
             }
             if (col > 0 && isOpen(row, col - 1)) {
-                uf.union(getIndex(row, col), getIndex(row, col - 1));
+                ufForP.union(getIndex(row, col), getIndex(row, col - 1));
+                ufForF.union(getIndex(row, col), getIndex(row, col - 1));
             }
             if (col < N - 1 && isOpen(row, col + 1)) {
-                uf.union(getIndex(row, col), getIndex(row, col + 1));
+                ufForP.union(getIndex(row, col), getIndex(row, col + 1));
+                ufForF.union(getIndex(row, col), getIndex(row, col + 1));
             }
             if (row == 0) {
-                uf.union(getIndex(row, col), topSite);
-            } else if (row == N - 1) {
-                uf.union(getIndex(row, col), bottomSite);
+                ufForP.union(getIndex(row, col), topSite);
+                ufForF.union(getIndex(row, col), topSite);
+            }
+            if (row == N - 1) {
+                ufForP.union(getIndex(row, col), bottomSite);
             }
         }
     }
@@ -74,7 +85,7 @@ public class Percolation {
     // is the site (row, col) full?
     public boolean isFull(int row, int col) {
         argumentCheck(row, col);
-        return uf.connected(getIndex(row, col), topSite);
+        return ufForF.connected(getIndex(row, col), topSite);
     }
     // number of open sites
     public int numberOfOpenSites() {
@@ -82,7 +93,7 @@ public class Percolation {
     }
     // does the system percolate?
     public boolean percolates() {
-        return uf.connected(topSite, bottomSite);
+        return ufForP.connected(topSite, bottomSite);
     }
     // use for unit testing (not required)
     public static void main(String[] args) {
@@ -91,16 +102,21 @@ public class Percolation {
         percolation.open(2, 4);
         percolation.open(2, 2);
         percolation.open(2, 3);
-//      percolation.open(0, 2);
+        percolation.open(0, 2);
         percolation.open(1, 2);
-        assertFalse(percolation.percolates());
         percolation.open(4, 4);
+        percolation.open(4, 0);
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 5; j++) {
                 System.out.print(percolation.items[i][j] + " ");
-                assertFalse(percolation.isFull(i, j));
             }
             System.out.println();
         }
+        System.out.println(percolation.percolates());
+        System.out.println(percolation.isFull(0, 4));
+        Percolation percolation1 = new Percolation(1);
+        percolation1.open(0, 0);
+        System.out.println(percolation1.items[0][0]);
+        System.out.println(percolation1.percolates());
     }
 }
