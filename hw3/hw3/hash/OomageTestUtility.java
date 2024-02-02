8275a4189da752f61c284fabc7457966fa6e6865
zf72 <zf72@duke.edu>
2024-02-02 13:37:12 -0500

    Day26: 110min

--- hw3/hw3/hash/OomageTestUtility.java
+++ hw3/hw3/hash/OomageTestUtility.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class OomageTestUtility {
     public static boolean haveNiceHashCodeSpread(List<Oomage> oomages, int M) {
-        /* TODO:
+        /*
          * Write a utility function that returns true if the given oomages
          * have hashCodes that would distribute them fairly evenly across
          * M buckets. To do this, convert each oomage's hashcode in the
@@ -12,6 +12,16 @@ public class OomageTestUtility {
          * and ensure that no bucket has fewer than N / 50
          * Oomages and no bucket has more than N / 2.5 Oomages.
          */
-        return false;
+        int[] distribution = new int[M];
+        for (int i = 0; i < oomages.size(); i++) {
+            int index = (oomages.get(i).hashCode() & 0x7FFFFFFF) % M;
+            distribution[index]++;
+        }
+        for (int i = 0; i < M; i++) {
+            if (distribution[i] < oomages.size() / 50 || distribution[i] > oomages.size() / 2.5) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- hw3/hw3/hash/TestComplexOomage.java
+++ hw3/hw3/hash/TestComplexOomage.java
@@ -33,18 +33,19 @@ public class TestComplexOomage {
         assertTrue(OomageTestUtility.haveNiceHashCodeSpread(oomages, 10));
     }
 
-    /* TODO: Create a list of Complex Oomages called deadlyList
-     * that shows the flaw in the hashCode function.
-     */
-    /*
     @Test
     public void testWithDeadlyParams() {
         List<Oomage> deadlyList = new ArrayList<>();
-
-        // Your code here.
-
+        List<Integer> tricks = new ArrayList<>();
+        tricks.add(1);
+        tricks.add(0);
+        tricks.add(0);
+        for (int i = 0; i < 8; i++) {
+            tricks.add(0);
+            deadlyList.add(new ComplexOomage(tricks));
+        }
         assertTrue(OomageTestUtility.haveNiceHashCodeSpread(deadlyList, 10));
-    } */
+    }
 
     /** Calls tests for SimpleOomage. */
     public static void main(String[] args) {
